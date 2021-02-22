d36eec69f0163567a026a5fbc1190123cab6dec6
liguo <liguo@panli.net>
2021-02-22 10:34:26 +0800

    code format

--- io-elves-core/src/main/java/io/elves/core/log/LogDriverInit.java
+++ io-elves-core/src/main/java/io/elves/core/log/LogDriverInit.java
@@ -3,7 +3,6 @@ package io.elves.core.log;
 import io.elves.core.ElvesConstants;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 
 
@@ -15,7 +14,7 @@ public class LogDriverInit {
 
     }
 
-    public static void init() throws IOException, URISyntaxException {
+    public static void init() throws URISyntaxException {
         String logConfigFileName = String.format("log4j2-%s.xml"
                 , System.getProperty(ElvesConstants.PROFILES_ACTIVE));
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
